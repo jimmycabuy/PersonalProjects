f3402be75a54f98ef0efb657ea8d3e54a681868b
Jimmy Cabuy <jimmycabuy@hotmail.com>
2022-06-21 10:09:32 +0200

    fix

--- uidesign/rentalcars/script.js
+++ uidesign/rentalcars/script.js
@@ -1,53 +1,51 @@
 function getDate() {
-    var today = new Date();
-    var dd = today.getDate();
-    var dd2 = today.getDate() + 7;
-    var mm = today.getMonth()+1;
-    var yyyy = today.getFullYear();
-    var hh = today.getHours()
-    var min = today.getMinutes()
-  
-    if(hh<10){
-        hh = '0'+hh
-    }
+  var today = new Date();
+  var dd = today.getDate();
+  var dd2 = today.getDate() + 7;
+  var mm = today.getMonth() + 1;
+  var yyyy = today.getFullYear();
+  var hh = today.getHours();
+  var min = today.getMinutes();
 
-    if(dd<10) {
-        dd = '0'+dd
-    } 
-
-    if(dd2<10) {
-        dd2 = '0'+dd2
-    } 
-  
-    if(mm<10) {
-        mm = '0'+mm
-    } 
-  
-    today = yyyy + '-' + mm + '-' + dd;
-    dateDelivery = yyyy + '-' + mm + '-' + dd2;
-    todayTime = hh + ':' + min;
-    document.getElementById("inputdate1").value = today;
-    document.getElementById("inputdate2").value = dateDelivery;
-    document.getElementById("inputdatetime1").value = todayTime;
-    document.getElementById("inputdatetime2").value = todayTime;
-    document.getElementById("inputdate1").min = today;
-    document.getElementById("inputdate2").min = today;
-
+  if (hh < 10) {
+    hh = "0" + hh;
   }
-  
-  window.onload = function() {
-    getDate();
-  };
 
-
-  function displayDeliveryLoc() {
-    console.log("bonjour");
-    var checkBox = document.getElementById("checkbox1");
-    var div = document.getElementById("divdeliverylocation");
-  
-    if (checkBox.checked == true){
-      div.style.display = "block";
-    } else {
-      div.style.display = "none";
-    }
+  if (dd < 10) {
+    dd = "0" + dd;
   }
+
+  if (dd2 < 10) {
+    dd2 = "0" + dd2;
+  }
+
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+
+  today = yyyy + "-" + mm + "-" + dd;
+  dateDelivery = yyyy + "-" + mm + "-" + dd2;
+  todayTime = hh + ":" + min;
+  document.getElementById("inputdate1").value = today;
+  document.getElementById("inputdate2").value = dateDelivery;
+  document.getElementById("inputdatetime1").value = todayTime;
+  document.getElementById("inputdatetime2").value = todayTime;
+  document.getElementById("inputdate1").min = today;
+  document.getElementById("inputdate2").min = today;
+}
+
+window.onload = function () {
+  getDate();
+};
+
+function displayDeliveryLoc() {
+  console.log("bonjour");
+  var checkBox = document.getElementById("checkbox1");
+  var div = document.getElementById("divdeliverylocation");
+
+  if (checkBox.checked == true) {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
+  }
+}
