41f7c00fde7ea5b6627ccbd1d8a748725ea367e7
Jimmy Cabuy <jimmycabuy@hotmail.com>
2022-07-24 13:03:29 +0200

    mcdonald design

--- mcdonald/src/components/ItemDown.jsx
+++ mcdonald/src/components/ItemDown.jsx
@@ -7,10 +7,10 @@ export default function ItemDown(props) {
           <img
             src={`/../../icons/${props.icon}`}
             alt={props.name}
-            className="img_item_up"
+            className="img_item_down"
           />
-          <h5>{props.name}</h5>
-          <p className='price'>$ {props.price}</p>
+          <h5 className='text_item_down'>{props.name}</h5>
+          <p className='price_item_down'>$ {props.price}</p>
         </div>
     </>
   )
