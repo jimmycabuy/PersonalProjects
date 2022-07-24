7a43d787315bd3325318d37bf51694783ad54be5
Jimmy Cabuy <jimmycabuy@hotmail.com>
2022-07-24 13:16:01 +0200

    new design exercice

--- mcdonald/src/components/ItemUpGrey.jsx
+++ mcdonald/src/components/ItemUpGrey.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function ItempUpGrey(props) {
   return (
     <>
-        <div className="item_up_grey">
-          <img
-            src={`/../../icons/${props.icon}`}
-            alt={props.name}
-            className="img_item_up"
-          />
-          <h5 className="text_item_up">{props.name}</h5>
-        </div>
+      <div className="item_up_grey">
+        <img
+          src={`/../../icons/${props.icon}`}
+          alt={props.name}
+          className="img_item_up"
+        />
+        <h5 className="text_item_up">{props.name}</h5>
+      </div>
     </>
   );
 }
